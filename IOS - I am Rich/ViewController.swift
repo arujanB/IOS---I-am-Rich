5bd47c0c4e4184db28448b2e4922a9bac510674b
arujanB <aruzhanboranbay@gmail.com>
2021-10-19 12:44:20 +0600

    Initial Commit

---
+++ IOS - I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS - I am Rich
+//
+//  Created by Aruzhan Boranbay on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
